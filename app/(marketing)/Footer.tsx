0b40d4506395e1d8e0a7ab347870a4c3340186fc
ramesh-kunwar <rameshkunwar1110@gmail.com>
2025-02-20 11:30:40 +0545

    Added flag on footer

--- app/(marketing)/Footer.tsx
+++ app/(marketing)/Footer.tsx
@@ -1,9 +1,32 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import React from "react";
 
 export const Footer = () => {
   return (
     <div className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size={"lg"} variant={"ghost"} className="w-full">
+          <Image
+            src={"/hr.svg"}
+            alt="Croatian"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Croatian
+        </Button>
+        <Button size={"lg"} variant={"ghost"} className="w-full">
+          <Image
+            src={"/jp.svg"}
+            alt="Japanese"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Japanese
+        </Button>
+      </div>
     </div>
   );
 };
