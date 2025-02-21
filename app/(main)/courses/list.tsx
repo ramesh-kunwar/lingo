884a17b219688a2c60ff2b0529f3c08b3a2297a0
ramesh-kunwar <rameshkunwar1110@gmail.com>
2025-02-21 15:36:48 +0545

    user progressed added

--- app/(main)/courses/list.tsx
+++ app/(main)/courses/list.tsx
@@ -1,13 +1,31 @@
 "use client";
 
-import { courses } from "@/db/schema";
+import { courses, userProgress } from "@/db/schema";
 import Card from "./card";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { upsertUserProgress } from "@/actions/user-progress";
+import { toast } from "sonner";
 
 type Props = {
   courses: (typeof courses.$inferSelect)[]; // wer are getting the type of the courses from the schema
-  activeCourseId: number;
+  activeCourseId?: (typeof userProgress.$inferSelect)["activeCourseId"];
 };
 const List = ({ courses, activeCourseId }: Props) => {
+  const router = useRouter();
+  const [pending, startTransition] = useTransition();
+
+  const onClick = (id: number) => {
+    if (pending) return;
+
+    if (id === activeCourseId) {
+      return router.push("/learn");
+    }
+
+    startTransition(() => {
+      upsertUserProgress(id).catch(() => toast.error("Something went wrong."));
+    });
+  };
   return (
     <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4">
       {courses.map((course) => {
@@ -18,7 +36,7 @@ const List = ({ courses, activeCourseId }: Props) => {
             title={course.title ?? ""}
             imageSrc={course.imageSrc ?? ""}
             disabled={false}
-            onClick={() => {}}
+            onClick={onClick}
             active={course.id === activeCourseId}
           />
         );
