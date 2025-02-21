dc4e7cf97b8080a7cc44d33fc0abf1601f4fbf4a
ramesh-kunwar <rameshkunwar1110@gmail.com>
2025-02-21 16:04:49 +0545

    Seed script added

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -2,12 +2,10 @@ import Link from "next/link";
 import { Button } from "./ui/button";
 import Image from "next/image";
 import { InfinityIcon } from "lucide-react";
+import { courses } from "@/db/schema";
 
 type Props = {
-  activeCourse: {
-    imageSrc: string;
-    title: string;
-  }; // replace with db types
+  activeCourse: typeof courses.$inferSelect;
   hearts: number;
   points: number;
   hasActiveSubscription: boolean;
@@ -24,8 +22,8 @@ const UserProgress = ({
       <Link href={"/courses"}>
         <Button variant={"ghost"}>
           <Image
-            src={activeCourse.imageSrc}
-            alt={activeCourse.title}
+            src={activeCourse.imageSrc || "/mascot.svg"}
+            alt={activeCourse.title || "Course"}
             className="rounded-md border"
             width={32}
             height={32}
